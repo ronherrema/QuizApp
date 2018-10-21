6aa170fa5e18d0b5bcb676a94a15f6531b63a332
Ron Herrema <rherrema@gmail.com>
2018-10-21 23:41:40 +0100

    Initial Commit

---
+++ QuizApp/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  QuizApp
+//
+//  Created by R J Herrema on 21/10/2018.
+//  Copyright © 2018 R J Herrema. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
