65b5973587c0de11481962a96aa85a855086dd8b
Ron Herrema <rherrema@gmail.com>
2018-10-27 23:00:52 +0100

    The UI has been rearranged and comments have been added.

--- QuizApp/Quiz.swift
+++ QuizApp/Quiz.swift
@@ -12,15 +12,18 @@ class Quiz {
     
     var questions: [String] = []
     var answers: [Int: Bool] = [:]
+    
+    // the initial current answer cannot be known and is thus an optional
     var currentAnswer: Bool?
     var questionNumber: Int = 0
     
-    
+    // instantiate the object using two parameters
     init(questions: [String], answers: [Int: Bool]) {
         self.answers = answers
         self.questions = questions
     }
     
+    // the function tells us whether the answers match
     func testCorrectness(answer: Bool) -> Bool {
         if answers[questionNumber] == answer {
             return true
--- QuizApp/ViewController.swift
+++ QuizApp/ViewController.swift
@@ -13,16 +13,18 @@ class ViewController: UIViewController {
 
         override func viewDidLoad() {
             super.viewDidLoad()
+            
             // pre-load the first question
             nextQuestion()
         }
 
-
+    // note that the members of the array can be put on different lines
     var myQuestions = ["The capital of Michigan is Lansing",
                        "Dolphins are fish",
                        "Grass is purple"]
     var myAnswers = [0: true, 1: false, 2: false]
     
+    // lazy instantiation solves the problem of 'self' not yet existing
     lazy var myQuiz = Quiz(questions: myQuestions, answers: myAnswers)
     
     
@@ -30,29 +32,32 @@ class ViewController: UIViewController {
     
     @IBOutlet weak var feedback: UILabel!
     
+    // one function for multiple buttons - True has the tag '1'
     @IBAction func testAnswer(_ sender: UIButton) {
         if sender.tag == 1 {
             myQuiz.currentAnswer = true
         } else {
             myQuiz.currentAnswer = false
         }
+        
+        // accessing a value in a dictionary gives back an optional - hence the forced unwrapping
         if myQuiz.testCorrectness(answer: myQuiz.currentAnswer!) {
             feedback.text = "correct, it's \(myQuiz.currentAnswer!)"
         } else {
             feedback.text = "incorrect"
         }
+        
         myQuiz.questionNumber += 1
         
         // prevent going out of bounds in the questions array
         if myQuiz.questionNumber == myQuestions.count {
             myQuiz.questionNumber = 0
         }
+        
         nextQuestion()
-       
     }
     
-    
-    
+    // load the next question and answer
     func nextQuestion() {
         questionField.text = myQuestions[myQuiz.questionNumber]
         myQuiz.currentAnswer = myAnswers[myQuiz.questionNumber]!
